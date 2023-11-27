cdfea79718fc6b6fe21e1ea1ce16de2a4fb05848
CANO <umutcanbozyigit34012@gmail.com>
2023-11-27 21:19:26 +0300

    mosh

---
+++ if.py
@@ -0,0 +1,8 @@
+sayi1=input("1.sayıyı giriniz: ")
+sayi2=input("2.sayıyı giriniz: ")
+if(sayi1<sayi2):
+    print("sayi2 büyüktür")
+elif(sayi1>sayi2):
+    print("sayi1 büyüktür")
+else:
+    print("sayılar eşittir...")        
