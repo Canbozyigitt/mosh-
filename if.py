29942ce61ad2e2034a154fd8ca5971a2f2c73254
Canbozyigitt <136346490+Canbozyigitt@users.noreply.github.com>
2023-12-30 14:03:47 +0300

    Update if.py

--- if.py
+++ if.py
@@ -3,6 +3,6 @@ sayi2=input("2.sayıyı giriniz: ")
 if(sayi1<sayi2):
     print("sayi2 büyüktür")
 elif(sayi1>sayi2):
-    print("sayi1 büyüktür")
+    print("sayi1 büyüktür..")
 else:
     print("sayılar eşittir...")        
